d0c669c0d4b978663d1db6868c026ec2dbe05cf9
kyanne-martins <kyanne801@gmail.com>
2022-09-13 00:17:25 -0300

    Conclusão Blog Pessoal

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -1,9 +1,9 @@
-import { UserLogin } from '../model/UserLogin';
+import { AlertasService } from './../service/alertas.service';
+import { environment } from './../../environments/environment.prod';
 import { Router } from '@angular/router';
-
-import { AuthService } from '../service/auth.service';
+import { AuthService } from './../service/auth.service';
+import { UserLogin } from './../model/UserLogin';
 import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-entrar',
@@ -13,29 +13,33 @@ import { environment } from 'src/environments/environment.prod';
 export class EntrarComponent implements OnInit {
 
   userLogin: UserLogin = new UserLogin()
-  constructor(private auth: AuthService, private router: Router) { }
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private alertas: AlertasService
+  ) { }
 
   ngOnInit() {
-    window.scroll(0, 0)
+    window.scroll(0,0)
   }
 
-  entrar() {
-    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin) => {
+  entrar(){
+    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin)=>{
       this.userLogin = resp
 
-      environment.token= this.userLogin.token
+      environment.token = this.userLogin.token
       environment.nome = this.userLogin.nome
       environment.foto = this.userLogin.foto
       environment.id = this.userLogin.id
       
+
       this.router.navigate(['/inicio'])
-    },
-      erro => {
-        if (erro.status == 500) {
-          alert('Usuário ou Senha incorreto.')
-        }
+    }, erro =>{
+      if(erro.status == 500){
+        this.alertas.showAlertDanger('Usuário ou senha estão incorretos!')
       }
-    )
+    })
   }
 
 }
